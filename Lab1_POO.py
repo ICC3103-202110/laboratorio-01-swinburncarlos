4acc4011af55e66cf0c6d1a0a15a1323848b8743
swinburncarlos <c(master)$ git config --global user.email>
2021-03-22 14:46:08 -0300

    first commit

---
+++ Lab1_POO.py
@@ -0,0 +1,12 @@
+# This is the first Lab of Object Oriented Programming
+# Monday March 22nd
+
+def user_input():
+    
+    cards = int(input('How many cards will the players have?'))
+    P1_name = input('What is the name of the first player?')
+    P2_name = input('What is the name of the second player?')
+    P1_puntos = 0
+    P2_putnos = 0
+    
+    return cards, P1_name, P2_name
