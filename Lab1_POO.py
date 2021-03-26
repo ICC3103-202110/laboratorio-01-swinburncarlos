446d8c794854453b10148a906faa4c4eeb60919e
swinburncarlos <c(master)$ git config --global user.email>
2021-03-25 21:58:31 -0300

    third commit

--- Lab1_POO.py
+++ Lab1_POO.py
@@ -3,36 +3,38 @@
 
 import numpy as np
 from numpy import *
-
-def user_input():
-    
-    cards = int(input('How many cards will the players have?'))
-    P1_name = input('What is the name of the first player?')
-    P2_name = input('What is the name of the second player?')
-    P1_points = 0
-    P2_points = 0
-    
-    return cards, P1_name, P2_name
-
-def game_setup(cards):
-    cards_P1 = range(cards)
-    cards_P2 = range(cards)
-
-    cards_P1.random.shuffle()
-    cards_P2.random.shuffle()
-
-    return cards_P1, cards_P2
+import math
 
 def matrix (cards):
     matrixlist=[]
     tuple1=()
     list1=[]
     list1=tuple(tuple1)
-    rows = cards
-    cols = cards
 
-    a = np.zeros((rows*cols))
-    for i in range(rows):
+    if cards < 10:
+        rows = 1
+        cols = cards
+        print(rows,cols)
+    elif cards >= 10:
+        rows = float(cards)/float(10)
+        print (rows) 
+        rowss = math.ceil(rows)
+        #print(rowss)
+        #leftover = cards - rows*10 
+        #cols = leftover 
+        cols=10
+        print(rowss,cols)
+
+    #rows = cards
+    #cols = cards
+    rows=int(rows)
+    rowss=int(rowss)
+    cards=int(cards)
+    cols=int(cols)
+    print(rows,rowss,cards,cols)
+
+    a = np.zeros((rowss*cols))
+    for i in range(rowss):
         list1=[]
         list1.append(i)
         t1=tuple(list1)
@@ -54,8 +56,63 @@ def matrix (cards):
 
     return m
 
-user_input()
+#def user_input():
+    
+cards = int(input('How many cards will the players have?  '))
+#P1_name = input('What is the name of the first player?')
+#P2_name = input('What is the name of the second player?')
+P1_points = 0
+P2_points = 0
+PointsP1 = 0
+PointsP2 = 0
+Turns = 0
+#return cards, P1_name, P2_name
+
+#def game_setup(cards):
+cards_P1 = range(cards)
+cards_P2 = range(cards)
+
+random.shuffle(cards_P1)
+random.shuffle(cards_P2)
+
+print (cards_P1)
+print (cards_P2)
+#    return cards_P1, cards_P2
+
+#user_input()
 #game_setup(user_input[0])
+
+mP1 = matrix(len(cards_P1))
+mP2 = matrix(len(cards_P2))
+dummy_number=0
+
+while PointsP1 < cards/2 or PointsP2 < cards/2:
+
+    if Turns % 2 == 0:
+        print(Turns)
+        x = input('Enter x coordinate:')
+        y = input('Enter y coordinate:')
+
+        mP1[x].insert(mP1[x].index((x, y)), cards_P1.pop(cards_P1[dummy_number]) )
+        mP1[x].pop(mP1[x].index((x, y)))
+        
+        for i in mP1:
+            print(i)
+
+        
+
+        PointsP1 +=1
+        dummy_number +=1
+        
+        
+
+    #else:
+        #x = input('Enter x coordinate:')
+        #y = input('Enter y coordinate:')
+    
+
+        #PointsP2 +=1
+
 
     
 
--- Lab1_POO_test.py
+++ Lab1_POO_test.py
@@ -1,5 +1,6 @@
 from numpy import *
 import pandas as pd
+from array import *
 #name = input()
 
 #print (name)
@@ -52,23 +53,65 @@ def matrix (cards):
 
     m=[data[i:i+cols] for i in range(0, len(data), cols)]
     #print(m)
+    #for i in m:
+    #    print(i)
+
+    return m
+
+
+def change_matrix ():
+#    m = matrix(4)
+    #lista1=[]
+    #lista2=[]
+    x = input('Enter x coordinate:')
+    y = input('Enter y coordinate:')
+    number=random.randint(1,cards+1)
+    list1=range(x)
+    list2=range(y)
+    random.shuffle(list1)
+    random.shuffle(list2)
+    print (list1)
+    print (list2)
+  
+#    number=str(' ')+str(number)+str('  ')
+    number = list1.insert(x, list)
+    m[x][y]=number
+
     for i in m:
         print(i)
 
     return m
 
-m = matrix(10)
+'''
+#def remember_change():
 
-number=random.randint(10)
-number=tuple(number)
-m[0][0]=number
-for i in m:
-    print(i)
+m = matrix(
+#change_matrix(matrix(4),0,1)
 
-#matrixlist = pd.DataFrame(matrixlist)
-#print(matrixlist)
+#def remember_change():
 
-#print(game_setup(10))
+i=0
+while i < 5:
+    change_matrix()
+    i+=1
+'''
 
+list1=[[(0, 0),(0, 1),(0, 2),(0, 3)],[(1, 0),(1, 1),(1, 2),(1, 3)]]
+list2=[0,1,2,3,4,5,6,7]
+dummy_number=1
 
+list2.insert(1,4)
+print(list2)
 
+print(list1)
+print(list2[1])
+position = list1[0].index((0, 1))
+#print(list1[0].index((0, 1)))
+print(position,'position')
+insert_num=list2.pop(list2[1])
+print(insert_num,'insert_num')
+
+list1[0].insert(position, insert_num )
+
+print(list1)
+print(list2)
